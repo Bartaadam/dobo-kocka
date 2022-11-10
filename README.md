f05e7ed03b629e6c82081cadaf0ce5b26134a9c3
Bartaadam <113516122+Bartaadam@users.noreply.github.com>
2022-11-10 09:17:13 +0100

    Kezdőfájlok a MakeCode projekthez

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Az oldal megnyitása itt: [https://bartaadam.github.io/dobo-kocka/](https://bartaadam.github.io/dobo-kocka/)
+
+## Használj egy bővítményt
+
+Ez a repó **bővítményként** használható a MakeCode-ban.
+
+* [https://makecode.microbit.org/](https://makecode.microbit.org/) megnyitása
+* kattints au **Új projekt**-re
+* kattints a **Bővítmények** gombra a fogaskerék menüben
+* **https://github.com/bartaadam/dobo-kocka** keresése, majd importálás
+
+## A projekt szerkesztése ![Projektbuild állapotát jelző ikon](https://github.com/bartaadam/dobo-kocka/workflows/MakeCode/badge.svg)
+
+A repó szerkesztése a MakeCode-ban.
+
+* [https://makecode.microbit.org/](https://makecode.microbit.org/) megnyitása
+* kattints az **Import**-ra, utána az **Import URL**-re
+* illeszd be a **https://github.com/bartaadam/dobo-kocka** címet és kattints az importra
+
+## Blokkok előnézete
+
+Ezen a képen az utolsó, fő ághoz tartozó verzió blokk kódja látható.
+A kép frissítése eltarthat néhány percig.
+
+![Renderelt nézet blokkokkal](https://github.com/bartaadam/dobo-kocka/raw/master/.github/makecode/blocks.png)
+
+#### Metaadatok (kereséshez, rendereléshez)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,89 @@
+let dobókocka = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    dobókocka = randint(1, 6)
+    if (dobókocka == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (dobókocka == 2) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    }
+    if (dobókocka == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    }
+    if (dobókocka == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (dobókocka == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (dobókocka == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
